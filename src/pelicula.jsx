85eb7d9aab38280f54f9a51999a00a299e121ca3
Guillermo <guillermogarnica94@gmail.com>
2024-10-13 17:53:46 +0200

    Eliminación archivos sobrantes

--- src/pelicula.jsx
+++
@@ -1,8 +0,0 @@
-function Pelicula(){
-    return(
-        <div class="pelicula">
-            <h3>Titulo</h3>
-            <button class="boton-mas">Más información</button>
-        </div>
-    );
-}
--- src/tarjeta.jsx
+++
@@ -1,7 +0,0 @@
-function Tarjeta(){
-    return(
-        <div>
-            Aquí va una película
-        </div>
-    );
-}
